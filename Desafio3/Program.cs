03103a27d3b3d6f6b51e81c3380b51eeef37e198
Tiago <tnthiagolima88@gmail.com>
2021-08-28 15:09:59 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divisores = 0;
+
+            Console.WriteLine("Entre com um Número Inteiro");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+                if(numero % i == 0)
+                    divisores++;
+                
+            if(divisores == 2)
+            {
+                Console.WriteLine("O "+ numero + " é Primo");
+            }
+            else
+            {
+                Console.WriteLine("O " + numero + " não e Primo");
+            }
+            
+        }
+    }
+}
